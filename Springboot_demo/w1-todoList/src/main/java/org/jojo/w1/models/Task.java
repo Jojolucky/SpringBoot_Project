33e0fcf0492ed684208a1fe43a415229d963152f
Jojo <jiaohelucky@gmail.com>
2024-07-31 15:31:51 -0700

    todolist backend

---
+++ Springboot_demo/w1-todoList/src/main/java/org/jojo/w1/models/Task.java
@@ -0,0 +1,22 @@
+package org.jojo.w1.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // this is the primary key which will be auto generated
+    private Long id;
+    private String task;
+    private boolean completed;
+}
