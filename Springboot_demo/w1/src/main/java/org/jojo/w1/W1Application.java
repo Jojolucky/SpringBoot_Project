6ec00f9f72e4676672451261088d48e8fc8ffdb5
Jojo <jiaohelucky@gmail.com>
2024-07-30 20:38:25 -0700

    w1

---
+++ Springboot_demo/w1/src/main/java/org/jojo/w1/W1Application.java
@@ -0,0 +1,13 @@
+package org.jojo.w1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(W1Application.class, args);
+    }
+
+}
